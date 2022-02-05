5b9db85aa7fa0bbac41b908cb804bbf971da3a8d
SmolPrzybs <maciek970@gmail.com>
2022-02-05 23:57:18 +0100

    Dodaj pliki projektów.

---
+++ Lab6z6/Lab6z6.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std; 
+
+void powtorz(char* wsk_a, int* wsk_b) {
+    for (int i = 0; i < *wsk_b; i++) {
+        cout << *wsk_a << " ";
+    }
+}
+
+int main()
+{
+    char a;
+    int b;
+    cin >> a;
+    cin >> b;
+
+    char* wsk_a = &a;
+    int* wsk_b = &b;
+
+    powtorz(wsk_a, wsk_b);
+}
